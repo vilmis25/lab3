2fd9f08e66401908c1ef7e939cebd830b2acdb88
DIRNEA-CTICD-VOLVERA <vilmis525@gmail.com>
2020-11-05 09:38:32 -0500

    Segunda prueba

---
+++ modules/core/test/com/company/lab3/Lab3TestContainer.java
@@ -0,0 +1,58 @@
+package com.company.lab3;
+
+import com.haulmont.cuba.testsupport.TestContainer;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Lab3TestContainer extends TestContainer {
+
+    public Lab3TestContainer() {
+        super();
+        //noinspection ArraysAsListWithZeroOrOneArgument
+        appComponents = new ArrayList<>(Arrays.asList(
+                // list add-ons here: "com.haulmont.reports", "com.haulmont.addon.bproc", etc.
+                "com.haulmont.cuba"
+        ));
+        appPropertiesFiles = Arrays.asList(
+                // List the files defined in your web.xml
+                // in appPropertiesConfig context parameter of the core module
+                "com/company/lab3/app.properties",
+                // Add this file which is located in CUBA and defines some properties
+                // specifically for test environment. You can replace it with your own
+                // or add another one in the end.
+                "com/company/lab3/test-app.properties");
+        autoConfigureDataSource();
+    }
+
+    
+
+    public static class Common extends Lab3TestContainer {
+
+        public static final Lab3TestContainer.Common INSTANCE = new Lab3TestContainer.Common();
+
+        private static volatile boolean initialized;
+
+        private Common() {
+        }
+
+        @Override
+        public void beforeAll(ExtensionContext extensionContext) throws Exception {
+            if (!initialized) {
+                super.beforeAll(extensionContext);
+                initialized = true;
+            }
+            setupContext();
+        }
+        
+
+        @SuppressWarnings("RedundantThrows")
+        @Override
+        public void afterAll(ExtensionContext extensionContext) throws Exception {
+            cleanupContext();
+            // never stops - do not call super
+        }
+        
+    }
+}
